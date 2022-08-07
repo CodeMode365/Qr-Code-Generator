d000fcd9c27e47d406f64515f7ceb2d01ab36ba6
CodeMode365 <codemode365@gmail.com>
2022-08-07 10:22:59 -0700

    qr generator completed

---
+++ QRgen.js
@@ -0,0 +1,20 @@
+let qrBtn = document.querySelector(".wrapper .form button");
+let wrapper = document.querySelector(".wrapper");
+let qrBody = document.querySelector('.wrapper .qr-code');
+let inputBox = document.querySelector('.wrapper .form input');
+let qrImg =  qrBody.querySelector('img');
+
+qrBtn.onclick=()=>{
+    //getting the users input
+    let data = inputBox.value;
+    //if the user input is empty then return noting
+    if(!data) return;
+    //when loading
+    qrBtn.innerText="Generating QR";
+    //api call to generate the qr image
+    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
+   qrImg.onload=()=>{
+    wrapper.classList.add('active');
+    qrBtn.innerText="Generate QR code";
+   }
+}
