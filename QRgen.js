68eff48830faebb2201a28ad39c2ed0a828100e5
CodeMode365 <codemode365@gmail.com>
2022-08-07 10:38:32 -0700

    enter button event added

--- QRgen.js
+++ QRgen.js
@@ -4,17 +4,26 @@ let qrBody = document.querySelector('.wrapper .qr-code');
 let inputBox = document.querySelector('.wrapper .form input');
 let qrImg =  qrBody.querySelector('img');
 
+
+onkeydown=(e)=>{
+if(e.key==="Enter"){
+    qrCall();
+}
+}
 qrBtn.onclick=()=>{
-    //getting the users input
-    let data = inputBox.value;
-    //if the user input is empty then return noting
-    if(!data) return;
-    //when loading
-    qrBtn.innerText="Generating QR";
-    //api call to generate the qr image
-    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
-   qrImg.onload=()=>{
-    wrapper.classList.add('active');
-    qrBtn.innerText="Generate QR code";
-   }
+  qrCall();
+}
+function qrCall(){
+      //getting the users input
+      let data = inputBox.value;
+      //if the user input is empty then return noting
+      if(!data) return;
+      //when loading
+      qrBtn.innerText="Generating QR";
+      //api call to generate the qr image
+      qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
+     qrImg.onload=()=>{
+      wrapper.classList.add('active');
+      qrBtn.innerText="Generate QR code";
+     }
 }
